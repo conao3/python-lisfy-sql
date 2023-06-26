8534a4da13398561b3b76979d0b698e0ed466e63
Naoya Yamashita <conao3@gmail.com>
2023-06-26 10:02:20 +0900

    implement read_token

--- src/lisfy_sql/reader.py
+++ src/lisfy_sql/reader.py
@@ -12,6 +12,34 @@ def consume_whitespace(input_stream: more_itertools.peekable) -> None:
         read_char(input_stream)
 
 
+def read_token(
+    input_stream: more_itertools.peekable,
+    eof_error_p: bool = True,
+    eof_value: Optional[str] = None,
+    recursive_p: bool = False,
+) -> Optional[str]:
+    consume_whitespace(input_stream)
+
+    peek: Optional[str] = input_stream.peek(None)
+
+    if peek is None:
+        if eof_error_p:
+            raise EOFError('unexpected EOF while reading token')
+
+        return eof_value
+
+    token = ''
+    while (peek := input_stream.peek(None)) is not None:
+        if peek.isspace():
+            break
+
+        char = read_char(input_stream)
+        assert char is not None
+        token += char
+
+    return token
+
+
 def read_char(
     input_stream: more_itertools.peekable,
     eof_error_p: bool = True,
@@ -35,4 +63,7 @@ def read(
     recursive_p: bool = False,
 ) -> types.Statement:
     consume_whitespace(input_stream)
+
+    token = read_token(input_stream)
+
     return types.Statement()
