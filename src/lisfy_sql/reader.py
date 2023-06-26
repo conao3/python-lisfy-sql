f07b511c303478d06656de8f543419ab97b68a62
Naoya Yamashita <conao3@gmail.com>
2023-06-26 09:49:22 +0900

    implement read_char

--- src/lisfy_sql/reader.py
+++ src/lisfy_sql/reader.py
@@ -1,6 +1,23 @@
+from typing import Optional
 import more_itertools
 
 from . import types
+
+
+def read_char(
+    input_stream: more_itertools.peekable,
+    eof_error_p: bool = True,
+    eof_value: Optional[str] = None,
+    recursive_p: bool = False,
+) -> Optional[str]:
+    peek: Optional[str] = input_stream.peek(None)
+    if peek is None:
+        if eof_error_p:
+            raise EOFError('unexpected EOF while reading character')
+
+        return eof_value
+    
+    return peek
 
 
 def read(
