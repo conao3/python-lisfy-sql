30ef929d05110acd371b1a505a64efe955ef0b40
Naoya Yamashita <conao3@gmail.com>
2023-06-26 14:09:38 +0900

    introduce UNKNOWN statement

--- src/lisfy_sql/types.py
+++ src/lisfy_sql/types.py
@@ -1,8 +1,17 @@
+import enum
 import pydantic
 
 
+class StatementEnum(enum.Enum):
+    SELECT = enum.auto()
+    INSERT = enum.auto()
+    UPDATE = enum.auto()
+    DELETE = enum.auto()
+    UNKNOWN = enum.auto()
+
+
 class Statement(pydantic.BaseModel):
-    pass
+    type_: StatementEnum = StatementEnum.UNKNOWN
 
 
 class Expression(pydantic.BaseModel):
